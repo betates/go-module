38ab6a6c03e0c4cf0c14fd31081d9a36d635c47a
betates <rofiq.beta@gmail.com>
2021-04-03 04:45:47 +0700

    Update Belajar Module With Parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/betates/go-module
+module github.com/betates/go-module/v2
 
 go 1.16
--- go_app.go
+++ go_app.go
@@ -1,5 +1,5 @@
 package go_app
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
